008fe113b96e29c748542edee19380729373dc8f
bedouijesser <bedouijesser@gmail.com>
2023-04-02 10:06:02 +0100

    updated content + implement Turbo for SPA Feel

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,2 +1,21 @@
 import * as turbo from '@hotwired/turbo'
 turbo.start()
+
+function beforeRender(event) {
+  document.removeEventListener('turbo:before-render', beforeRender)
+  event.preventDefault();
+  if (!document.startViewTransition) {
+    event.detail.resume()
+    console.debug("Your browser doesn't support the View Transition API")
+    // View Transition API not supported, fallback
+    return;
+  }
+  document.startViewTransition(() => {
+    event.detail.resume();
+  });
+}
+
+
+document.addEventListener('turbo:load', () => {
+  document.addEventListener('turbo:before-render', beforeRender)
+})
